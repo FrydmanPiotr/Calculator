e7396d13f2adb45660de6a05d68484823fd75cf7
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2023-06-16 10:04:02 +0200

    Aktualizacja programu Kalkulator
    Zastąpienie funkcji instrukcją warunkową if

--- calculator.py
+++ calculator.py
@@ -3,11 +3,10 @@
 """
 from tkinter import Tk, Button, Label, Entry
 import operator
-import sys
 
 calc = Tk()
 calc.title("Kalkulator")
-calc.geometry('600x400')
+calc.geometry('400x450')
 
 l1 = Label(calc, text = "1. liczba")
 l1.pack()
@@ -19,55 +18,28 @@ l2.pack()
 e2 = Entry(calc, bd =3)
 e2.pack()
 
-"""Wykonywanie operacji matematycznych: dodawanie, odjemowanie, mnożenie i dzielenie."""
-def add():
+def calculate(operation):
     try:
         a = int(e1.get())
         b = int(e2.get())
-        result = operator.add(a, b)
-        label.config(text=result)
-    except ValueError:
-        label.config(text="Proszę podać 2 liczby.")
-
-def sub():
-    try:
-        a = int(e1.get())
-        b = int(e2.get())
-        result = (operator.sub(a, b))
-        label.config(text=result)
-    except ValueError:
-        label.config(text="Proszę podać 2 liczby.")
-
-def mul():
-    try:
-        a = int(e1.get())
-        b = int(e2.get())
-        result = (operator.mul(a, b))
-        label.config(text=result)
-    except ValueError:
-        label.config(text="Proszę podać 2 liczby.")
-
-def div():
-    try:
-        a = int(e1.get())
-        b = int(e2.get())
-        result = (operator.truediv(a, b))
+        if operation == "+":
+            result = operator.add(a, b)
+        elif operation == "-":
+            result = operator.sub(a, b)
+        elif operation == "*":
+            result = operator.mul(a, b)
+        elif operation == "/":
+            result = operator.truediv(a, b)
         label.config(text=result)
     except ZeroDivisionError:
-        label.config(text="Nie wolno dzielić przez zero !")
+        label.config(text="Nie wolno dzielić przez zero!")
     except ValueError:
         label.config(text="Proszę podać 2 liczby.")
 
-
-def exit_button():
-    sys.exit()
-
-"Przypisywanie funkcji do przycisków."
-exit_button = Button(calc, text="WYJŚCIE", width=12, command=exit_button)
-add_button = Button(calc, text="Dodawanie", width=12, command=add)
-sub_button = Button(calc, text="Odejmowanie", width=12, command=sub)
-mul_button = Button(calc, text="Mnożenie", width=12, command=mul)
-div_button = Button(calc, text="Dzielenie", width=12, command=div)
+add_button = Button(calc, text="+", width=6,command=lambda: calculate("+"))
+sub_button = Button(calc, text="-", width=6, command=lambda: calculate("-"))
+mul_button = Button(calc, text="*", width=6, command=lambda: calculate("*"))
+div_button = Button(calc, text="/", width=6, command=lambda: calculate("/"))
 
 add_button.pack()
 sub_button.pack()
@@ -79,9 +51,4 @@ result_label.pack(pady=10)
 label = Label(calc)
 label.pack(pady=5)
 
-exit_button.pack(pady=8)
-
 calc.mainloop()
-
-
-
